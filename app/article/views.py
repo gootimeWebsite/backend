fa2f1a3fb5419ff43d45f9dd7606101cb7022d59
liziming5353 <45557016+liziming5353@users.noreply.github.com>
2019-02-10 22:24:40 +0800

    docs: finish article api docs annotation

--- app/article/views.py
+++ app/article/views.py
@@ -13,12 +13,50 @@ class ArticleInfo(Resource):
     '''
     lzm完成
     '''
+
+    
+
+    """
+    @api {get} /forum/:id Get Article Post
+    @apiVersion 0.1.0
+    @apiName GetArticleID
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to get a post.
+
+    @apiUse Authorization
+
+    @apiSuccess {String} data Post's data.
+    @apiSuccess {String} data.auther Post's auther.
+    @apiSuccess {String} data.content Post's content.
+    @apiSuccess {String} data.id Post's id.
+    @apiSuccess {String} data.title Post's title.
+    @apiSuccess {String} data.category Post's category.
+    @apiSuccess {String} data.updatetime Post's updatetime.
+    @apiSuccess {String} message Post's getting status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "data": {
+                "auther": "Tel72250567",
+                "title": "Test",
+                "content": "This is a test!",
+                "category": "test"
+                "updatetime": "Sun, 03 Feb 2019 04:26:31 GMT"
+                "id": "511053",
+            },
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse PostNotFoundError
+    """
     def get(self,id):
         ret = {}
         ret_article={}
         post = Article.query.filter_by(id=id).first()
         if post is None:
-            ret['message'] = "not found error"
+            ret['message'] = "post not found"
             status = 404
         else:
             ret_article['auther'] = post.auther
@@ -36,14 +74,44 @@ class ArticleInfo(Resource):
         response.status_code = status
         return response
         
+    """
+    @api {put} /forum/:id Update Article Post
+    @apiVersion 0.1.0
+    @apiName PutArticleID
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to update a post.
 
+    @apiUse Authorization
+    @apiParam {String} title New title of the post.
+    @apiParam {String} content New content of the post.
+    @apiParam {String} auther New auther of the post.
+    @apiParam {String} category New category of the post.
+    @apiSuccess {String} data Post's data.
+    @apiSuccess {String} data.auther Post's auther.
+    @apiSuccess {String} data.content Post's content.
+    @apiSuccess {String} data.id Post's id.
+    @apiSuccess {String} data.title Post's title.
+    @apiSuccess {String} data.category Post's category.
+    @apiSuccess {String} data.updatetime Post's updatetime.
+    @apiSuccess {String} message Post's putting status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse PostNotFoundError
+    @apiUse InvalidRequestError
+    """
     def put(self,id):
         ret = {}
         data = json.loads(request.get_data())
         ret_article={}
         post = Article.query.filter_by(id=id).first()
         if post is None:
-            ret['message'] = "request error"
+            ret['message'] = "post not found"
             status = 404
         else:
             try:
@@ -63,7 +131,8 @@ class ArticleInfo(Resource):
                 ret['data'] = ret_article
                 ret['message'] = "success"
             except:
-                ret['message'] = "put error"
+                ret['message'] = "invalid request"
+                ret['error'] = "InvalidRequest"
                 status = 400
 
         response = make_response(json.dumps(ret))
@@ -71,13 +140,44 @@ class ArticleInfo(Resource):
         response.status_code = status
         return response
 
+"""
+    @api {patch} /forum/:id Update Article Post
+    @apiVersion 0.1.0
+    @apiName PatchArticleID
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to update a part of a post.
+
+    @apiUse Authorization
+    @apiParam {String} title New title of the post.
+    @apiParam {String} content New content of the post.
+    @apiParam {String} auther New auther of the post.
+    @apiParam {String} category New category of the post.
+    @apiSuccess {String} data Post's data.
+    @apiSuccess {String} data.auther Post's auther.
+    @apiSuccess {String} data.content Post's content.
+    @apiSuccess {String} data.id Post's id.
+    @apiSuccess {String} data.title Post's title.
+    @apiSuccess {String} data.category Post's category.
+    @apiSuccess {String} data.updatetime Post's updatetime.
+    @apiSuccess {String} message Post's patching status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse PostNotFoundError
+    @apiUse InvalidRequestError
+    """
     def patch(self,id):
         ret = {}
         data = json.loads(request.get_data())
         ret_article={}
         post = Article.query.filter_by(id=id).first()
         if post is None:
-            ret['message'] = "request error"
+            ret['message'] = "post not found"
             status = 404
         else:
             try:
@@ -102,7 +202,8 @@ class ArticleInfo(Resource):
                 ret['data'] = ret_article
                 ret['message'] = "success"
             except:
-                ret['message'] = "patch error"
+                ret['message'] = "invalid request"
+                ret['error'] = "InvalidRequest"
                 status = 400
 
         response = make_response(json.dumps(ret))
@@ -110,12 +211,33 @@ class ArticleInfo(Resource):
         response.status_code = status
         return response
 
+"""
+    @api {delete} /forum/:id Delete Article Post
+    @apiVersion 0.1.0
+    @apiName DeleteArticleID
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to delete a post.
+
+    @apiUse Authorization
+
+    @apiSuccess {String} message Post's deleting status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse PostNotFoundError
+    @apiUse InvalidRequestError
+    """
     def delete(self,id):
         ret = {}
 
         post = Article.query.filter_by(id=id).first()
         if post is None:
-            ret['message'] = "not found error"
+            ret['message'] = "post not found"
             status = 404
         else:
             try:
@@ -124,7 +246,8 @@ class ArticleInfo(Resource):
                 ret['message'] = "success"
                 status = 200
             except:
-                ret['message'] = "delete error"
+                ret['message'] = "invalid request"
+                ret['error'] = "InvalidRequest"
                 status = 400
 
         response = make_response(json.dumps(ret))
@@ -136,12 +259,58 @@ class ArticleInfo(Resource):
 @api.resource('/')
 class Article1(Resource):
     decorators = [auth.login_required]
+
+    """
+    @api {get} /article Get article homepage
+    @apiVersion 0.1.0
+    @apiName GetArticle
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to get article homepage in which the posts are sorted in chronological order.
+
+    @apiUse Authorization
+
+    @apiSuccess {String} data Post's data list.
+    @apiSuccess {String} data[i].auther Post's auther.
+    @apiSuccess {String} data[i].content Post's content.
+    @apiSuccess {String} data[i].id Post's id.
+    @apiSuccess {String} data[i].title Post's title.
+    @apiSuccess {String} data[i].category Post's category.
+    @apiSuccess {String} data[i].updatetime Post's updatetime.
+    @apiSuccess {String} message Post's getting status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "data": [
+                {
+                    "auther": "Tel72250567",
+                    "title": "Test",
+                    "content": "This is a test!",
+                    "category": "test"
+                    "updatetime": "Sun, 03 Feb 2019 04:26:31 GMT"
+                    "id": "511053",
+                },
+                {
+                    "auther": "Tel72250568",
+                    "title": "Test",
+                    "content": "This is a test!",
+                    "category": "test"
+                    "updatetime": "Sun, 03 Feb 2019 04:26:42 GMT"
+                    "id": "511057",
+                }
+            ],
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse PostNotFoundError
+    """
     def get(self):
         ret = {}
         ret_article=[]
         post = Article.query.all()
         if post is None or post == []:
-            ret['message'] = "not found error"
+            ret['message'] = "post not found"
             status = 404
         else:
             ret['message'] = "success"
@@ -154,6 +323,40 @@ class Article1(Resource):
         response.headers['Content-Type'] = 'application/json;charset=utf8'
         response.status_code = status
         return response
+
+    """
+    @api {post} /forum Create A New Article Post
+    @apiVersion 0.1.0
+    @apiName PostArticle
+    @apiGroup Article
+    @apiPermission User
+    @apiDescription API for user to create a new post.
+
+    @apiUse Authorization
+    @apiParam {String} title The title of the post.
+    @apiParam {String} content The content of the post.
+    @apiParam {String} title New title of the post.
+    @apiParam {String} content New content of the post.
+    @apiParam {String} auther New auther of the post.
+    @apiParam {String} category New category of the post.
+    @apiSuccess {String} data Post's data.
+    @apiSuccess {String} data.auther Post's auther.
+    @apiSuccess {String} data.content Post's content.
+    @apiSuccess {String} data.id Post's id.
+    @apiSuccess {String} data.title Post's title.
+    @apiSuccess {String} data.category Post's category.
+    @apiSuccess {String} data.updatetime Post's updatetime.
+    @apiSuccess {String} message Post's creation status: 'success'.
+    @apiSuccessExample {json} Success-Response:
+        HTTP/1.1 201 OK
+        {
+            "id": "901350",
+            "message": "success"
+        }
+
+    @apiUse UnauthorizedError
+    @apiUse InvalidRequestError
+    """
     def post(self):
         ret = {}
         ret_article={}
@@ -179,7 +382,8 @@ class Article1(Resource):
             status = 201
         except:
             status = 400
-            ret['message'] = "post error"
+            ret['message'] = "invalid request"
+            ret['error'] = "InvalidRequest"
 
         response = make_response(json.dumps(ret))
         response.headers['Content-Type'] = 'application/json;charset=utf8'
